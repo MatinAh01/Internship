88886696e138990e099257b07772b624657160bb
MatinAh01 <mtnahmady01@gmail.com>
2025-02-04 20:31:07 +0330

    add new repo

---
+++ Session_2/thread/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Thread t = new(WriteT);
+        t.Start();
+
+        for (int i = 0; i < 100; i++)
+        {
+            Console.Write("M ");
+        }
+    }
+    public static void WriteT()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            Console.Write("T ");
+        }
+    }
+}
